407d7e312ab538e569c28f3e0323f622e98e8371
José <Jose.Paumard@gmail.com>
2015-08-19 11:08:12 +0200

    Work on Producing home made beans

--- code/src/main/java/chap04/repository/InMemoryPostRepository.java
+++ code/src/main/java/chap04/repository/InMemoryPostRepository.java
@@ -8,7 +8,14 @@ import java.util.Map;
 public class InMemoryPostRepository implements PostRepository {
 
     private long id = 0L;
-    private Map<Long, Post> posts = new HashMap<>();
+    private Map<Long, Post> posts;
+
+    public InMemoryPostRepository() {
+    }
+
+    public InMemoryPostRepository(int size) {
+        posts = new HashMap<>(size);
+    }
 
     @Override
     public void save(Post post) {
